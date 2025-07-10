8e0a06d6515f764019dc02c8cebb079a6585786f
EmersonHoruss <peralesvillanuevaemerson@gmail.com>
2025-07-10 13:37:20 -0500

    logging

--- amplify/backend/function/test2triggers/src/index.js
+++ amplify/backend/function/test2triggers/src/index.js
@@ -14,12 +14,13 @@ Amplify Params - DO NOT EDIT */
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
-exports.handler = event => {
+exports.handler = (event) => {
   console.log(`EVENT: ${JSON.stringify(event)}`);
   for (const record of event.Records) {
-    console.log(record.eventID);
-    console.log(record.eventName);
-    console.log('DynamoDB Record: %j', record.dynamodb);
+    console.log("record event id", record.eventID);
+    console.log("record event name", record.eventName);
+    console.log("DynamoDB Record: %j", record.dynamodb);
+    console.log("record", record);
   }
-  return Promise.resolve('Successfully processed DynamoDB record');
+  return Promise.resolve("Successfully processed DynamoDB record");
 };
