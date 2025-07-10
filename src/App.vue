548596a6acb3b704cf6258448f62adc11d240cd7
EmersonHoruss <peralesvillanuevaemerson@gmail.com>
2025-07-10 13:33:19 -0500

    working router

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <Header />
+    <main>
+      <router-view />
+    </main>
+  </div>
+</template>
+
+<script setup>
+import Header from "./components/Header.vue";
+</script>
+
+<style>
+body {
+  margin: 0;
+  font-family: Arial, sans-serif;
+}
+main {
+  padding: 1rem;
+}
+</style>
